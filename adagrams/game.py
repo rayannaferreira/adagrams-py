6c66f06056eedfe77066ac2f0987bafd2d561119
Rayanna Ferreira <rayanna_ferreira@icloud.com>
2026-09-17 17:38:18 -0700

    address feedback and fix draw letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -11,26 +11,20 @@ LETTER_SCORES = {
     "R": 1,
     "S": 1,
     "T": 1,
-
     "D": 2,
     "G": 2,
-
     "B": 3,
     "C": 3,
     "M": 3,
     "P": 3,
-
     "F": 4,
     "H": 4,
     "V": 4,
     "W": 4,
     "Y": 4,
-
     "K": 5,
-
     "J": 8,
     "X": 8,
-
     "Q": 10,
     "Z": 10
 }
@@ -64,34 +58,32 @@ LETTER_POOL = {
     'Z': 1
 }
 
-ALPHABET = list(LETTER_POOL.keys())
 
 def draw_letters():
-    letters = []   # create an empty list
-    copia = LETTER_POOL.copy() # make a copy of the pool
-    while len(letters) < 10: # while I wont fullfil all the letters
-        position = randint(0, 25) # sort a position
-        letter = ALPHABET[position]   # obtains the letter according with the sorted position
-        if copia[letter] > 0: #first we verify if the letter is avaliable then if is avaliable we add in the it on the list with append
-                letters.append(letter)
-                copia[letter] -=1 
+    letters = []   
+    pool = []
+
+    for letter, quantity in LETTER_POOL.items():
+        for _ in range(quantity):
+            pool.append(letter)
+    while len(letters) < 10: 
+        position = randint(0, len(pool) - 1)
+        letter = pool[position]
+        letters.append(letter)
+        pool.pop(position)   
+        
     return letters
     
 
 def uses_available_letters(word, letter_bank): 
-    word= word.upper()#convert all leters in a string uppercase 
-    frequences = {} # create a frequency dictionary
+    frequences = {} 
     for letter in letter_bank:
-        if letter in frequences:
-                frequences[letter] +=1
-        else:
-                frequences[letter] =1
+        frequences[letter] = frequences.get(letter, 0) + 1
 
-    # verify if there is the letter and if it respects the frequency
-    for letter in word:
+    for letter in word.upper():
         if letter in frequences:
-            if frequences[letter] >0:
-                frequences[letter] -=1
+            if frequences[letter] > 0:
+                frequences[letter] -= 1
             else:
                 return False
         else:
@@ -101,27 +93,26 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    word= word.upper()#convert all leters in a string uppercase 
-    score= 0
-    for letter in word:
-        if letter in LETTER_SCORES:#if the letter is in LETTER_SCORES, add its score
+    score = 0
+    for letter in word.upper():
+        if letter in LETTER_SCORES:
             score += LETTER_SCORES[letter]
-    if len(word) >= 7 and len(word) <= 10:# Check if the word has between 7 and 10 letters
-        score +=8
+    if len(word) >= 7 and len(word) <= 10:
+        score += 8
     return score
 
 
 def get_highest_word_score(word_list):
-    winning_word= ""
-    winning_score= 0
-    for word in word_list:# Check each word in the list
+    winning_word = ""
+    winning_score = 0
+    for word in word_list:
         if score_word(word) >  winning_score:
             winning_score = score_word(word)
             winning_word = word
             
-        elif score_word (word)== winning_score:
-            if len(word) == 10 and len(winning_word)!= 10:# Prefer a 10 letter word when the scores are the same
-                winning_word = word # Update the winning word
+        elif score_word (word) == winning_score:
+            if len(word) == 10 and len(winning_word)!= 10:
+                winning_word = word 
             elif len(word) < len(winning_word) and len(winning_word)!= 10:
                 winning_word = word
 
